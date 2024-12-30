40b3d221615658775b1b656e104901026f0c07fd
LIG-JY <dldlsry053@naver.com>
2024-12-30 22:18:09 +0900

    Day13

--- src/Day13_part1.kt
+++ src/Day13_part1.kt
@@ -1,2 +1,101 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
 class Day13_part1 {
+    fun main() {
+        BufferedReader(InputStreamReader(System.`in`)).use { reader ->
+            BufferedWriter(OutputStreamWriter(System.out)).use { writer ->
+                val input = reader.readText()
+
+                data class Point(val x: Int, val y: Int) {
+                    operator fun plus(other: Point) = Point(x + other.x, y + other.y)
+                }
+
+                var a = Point(Int.MAX_VALUE, Int.MAX_VALUE)
+                var b = Point(Int.MAX_VALUE, Int.MAX_VALUE)
+                var prize = Point(Int.MAX_VALUE, Int.MAX_VALUE)
+
+                fun parseButtonInput(input: String): Point {
+                    val (x, y) = input.split("X+", "Y+", ",")
+                        .filter { it.trim().toIntOrNull() != null }.map { it.toInt() }
+
+                    return Point(x, y)
+                }
+
+                fun parsePrizeInput(input: String): Point {
+                    val (x, y) = input.split("X=", "Y=", ",")
+                        .filter { it.trim().toIntOrNull() != null }.map { it.toInt() }
+
+                    return Point(x, y)
+                }
+
+
+                data class Node(val p: Point, val cost: Int, val counts: Pair<Int, Int>)
+
+                fun solveOrNull(a: Point, b: Point, prize: Point): Int? {
+                    val start = Node(Point(0, 0), 0, 0 to 0)
+                    val A = Node(a, 3, 0 to 0)
+                    val B = Node(b, 1, 0 to 0)
+
+
+                    val pq = PriorityQueue<Node>(compareBy { it.cost })
+                    val visited = mutableMapOf<Point, Int>()
+                    pq.add(start)
+
+                    while (pq.isNotEmpty()) {
+                        val cur = pq.poll()
+
+                        if (cur.p == prize) return cur.cost
+
+                        if (cur.counts.first > 100 || cur.counts.second > 100) {
+                            continue
+                        }
+
+                        if (visited[cur.p]?.let { it <= cur.cost } == true) {
+                            continue
+                        }
+
+                        visited[cur.p] = cur.cost
+                        pq.add(Node(cur.p + A.p, cur.cost + A.cost, cur.counts.first + 1 to cur.counts.second))
+                        pq.add(Node(cur.p + B.p, cur.cost + B.cost, cur.counts.first to cur.counts.second + 1))
+                    }
+
+                    return null
+                }
+
+                var sum = 0
+                input.split(System.lineSeparator()).forEachIndexed { index, s ->
+                    val mod = index % 4
+                    when (mod) {
+                        0 -> {
+                            a = parseButtonInput(s)
+                        }
+
+                        1 -> {
+                            b = parseButtonInput(s)
+                        }
+
+                        2 -> {
+                            prize = parsePrizeInput(s)
+                        }
+
+                        3 -> {
+                            solveOrNull(a, b, prize)?.let {
+                                sum += it
+                            }
+                        }
+                    }
+                }
+                writer.writeLine(sum)
+            }
+        }
+    }
+
+    private fun BufferedWriter.writeLine(v: Any) {
+        this.write("$v")
+        this.newLine()
+    }
 }
--- src/Day13_part2.kt
+++ src/Day13_part2.kt
@@ -1,2 +1,84 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.math.BigInteger
+
 class Day13_part2 {
+    fun main() {
+        BufferedReader(InputStreamReader(System.`in`)).use { reader ->
+            BufferedWriter(OutputStreamWriter(System.out)).use { writer ->
+                val input = reader.readText()
+
+                data class Point(val x: BigInteger, val y: BigInteger)
+
+                var a = Point(BigInteger.ZERO, BigInteger.ZERO)
+                var b = Point(BigInteger.ZERO, BigInteger.ZERO)
+                var prize = Point(BigInteger.ZERO, BigInteger.ZERO)
+
+                fun parseButtonInput(input: String): Point {
+                    val (x, y) = input.split("X+", "Y+", ",")
+                        .filter { it.trim().toBigIntegerOrNull() != null }.map { it.toBigInteger() }
+
+                    return Point(x, y)
+                }
+
+                fun parsePrizeInput(input: String): Point {
+                    val (x, y) = input.split("X=", "Y=", ",")
+                        .filter { it.trim().toBigIntegerOrNull() != null }
+                        .map { it.toBigInteger() + BigInteger("10000000000000") }
+
+                    return Point(x, y)
+                }
+
+                fun solveOrNull(a: Point, b: Point, prize: Point): BigInteger? {
+                    var det = a.x * b.y - a.y * b.x
+                    val detSign = det.signum().toBigInteger()
+                    det *= detSign  // ensure det > 0
+
+                    val na = (prize.x * b.y - prize.y * b.x) * detSign  // numerator
+                    val nb = (a.x * prize.y - a.y * prize.x) * detSign  // numerator
+                    if (na.mod(det) != BigInteger.ZERO) return null // solution must be integer
+                    if (nb.mod(det) != BigInteger.ZERO) return null // solution must be integer
+                    if (na > BigInteger.ZERO && nb > BigInteger.ZERO) { // solution must be positive
+                        val sa = (na.div(det))
+                        val sb = (nb.div(det))
+
+                        return BigInteger("3").multiply(sa).add(sb)
+                    }
+                    return null
+                }
+
+                var sum = BigInteger.ZERO
+                input.split(System.lineSeparator()).forEachIndexed { index, s ->
+                    val mod = index % 4
+                    when (mod) {
+                        0 -> {
+                            a = parseButtonInput(s)
+                        }
+
+                        1 -> {
+                            b = parseButtonInput(s)
+                        }
+
+                        2 -> {
+                            prize = parsePrizeInput(s)
+                        }
+
+                        3 -> {
+                            solveOrNull(a, b, prize)?.let {
+                                sum += it
+                            }
+                        }
+                    }
+                }
+                writer.writeLine(sum)
+            }
+        }
+    }
+
+    private fun BufferedWriter.writeLine(v: Any) {
+        this.write("$v")
+        this.newLine()
+    }
 }
